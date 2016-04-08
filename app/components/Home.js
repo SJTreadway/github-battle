d53056a753925012b83575b45410843459502cb3
SJTreadway <treadway08@gmail.com>
2016-04-08 09:47:22 -0500

    initial commit. files broken because of location.

---
+++ app/components/Home.js
@@ -0,0 +1,22 @@
+var React = require('react');
+var transparentBG = require('../styles').transparentBG;
+var ReactRouter = require('react-router');
+var Link = ReactRouter.Link;
+
+var Home = React.createClass({
+    render() {
+        return (
+            <div 
+                className="jumbotron col-sm-12 text-center" 
+                style={transparentBG}>
+                <h1>Github Battle</h1>
+                <p className="lead">Some fancy motto</p>
+            <Link to="/playerOne">
+                <button className="btn btn-lg btn-success">Get Started</button>
+            </Link>
+            </div>
+        )
+    }
+});
+
+module.exports = Home;
